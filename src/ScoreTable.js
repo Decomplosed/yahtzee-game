3ebd33589b2076f3f260f78fdb2b4832a855b9fc
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-12 21:23:45 +0100

    added description prop to all RuleRows

--- src/ScoreTable.js
+++ src/ScoreTable.js
@@ -28,31 +28,37 @@ class ScoreTable extends Component {
           <table cellSpacing='0'>
             <tbody>
               <RuleRow
+                description={ones.description}
                 name='Ones'
                 score={scores.ones}
                 doScore={evt => doScore('ones', ones.evalRoll)}
               />
               <RuleRow
+                description={twos.description}
                 name='Twos'
                 score={scores.twos}
                 doScore={evt => doScore('twos', twos.evalRoll)}
               />
               <RuleRow
+                description={threes.description}
                 name='Threes'
                 score={scores.threes}
                 doScore={evt => doScore('threes', threes.evalRoll)}
               />
               <RuleRow
+                description={fours.description}
                 name='Fours'
                 score={scores.fours}
                 doScore={evt => doScore('fours', fours.evalRoll)}
               />
               <RuleRow
+                description={fives.description}
                 name='Fives'
                 score={scores.fives}
                 doScore={evt => doScore('fives', fives.evalRoll)}
               />
               <RuleRow
+                description={sixes.description}
                 name='Sixes'
                 score={scores.sixes}
                 doScore={evt => doScore('sixes', sixes.evalRoll)}
@@ -65,21 +71,25 @@ class ScoreTable extends Component {
           <table cellSpacing='0'>
             <tbody>
               <RuleRow
+                description={threeOfKind.description}
                 name='Three of Kind'
                 score={scores.threeOfKind}
                 doScore={evt => doScore('threeOfKind', threeOfKind.evalRoll)}
               />
               <RuleRow
+                description={fourOfKind.description}
                 name='Four of Kind'
                 score={scores.fourOfKind}
                 doScore={evt => doScore('fourOfKind', fourOfKind.evalRoll)}
               />
               <RuleRow
+                description={fullHouse.description}
                 name='Full House'
                 score={scores.fullHouse}
                 doScore={evt => doScore('fullHouse', fullHouse.evalRoll)}
               />
               <RuleRow
+                description={smallStraight.description}
                 name='Small Straight'
                 score={scores.smallStraight}
                 doScore={evt =>
@@ -87,6 +97,7 @@ class ScoreTable extends Component {
                 }
               />
               <RuleRow
+                description={largeStraight.description}
                 name='Large Straight'
                 score={scores.largeStraight}
                 doScore={evt =>
@@ -94,11 +105,13 @@ class ScoreTable extends Component {
                 }
               />
               <RuleRow
+                description={yahtzee.description}
                 name='Yahtzee'
                 score={scores.yahtzee}
                 doScore={evt => doScore('yahtzee', yahtzee.evalRoll)}
               />
               <RuleRow
+                description={chance.description}
                 name='Chance'
                 score={scores.chance}
                 doScore={evt => doScore('chance', chance.evalRoll)}
